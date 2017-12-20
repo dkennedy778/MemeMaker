16f9251f03c60be3a94a48bc96eef79dd54750cd
Derek Kennedy <derek.kennedy.13@cnu.edu>
2017-12-20 16:19:33 -0500

    finished prototype of memeGenerator. Next step is to link the parser to the generator and run a full batch of test files

--- MemeMaker/JSONParser.py
+++ MemeMaker/JSONParser.py
@@ -8,38 +8,40 @@ from pprint import pprint
 namesList = []
 substring_list = [".jpg","pic","imgur"]
 
-with open('sampleData.json') as data_file:
+with open('memeData1218To1219') as data_file:
     data = json.load(data_file)
     #pprint(data)
 #first lets get the names of all users
-names = []
-for dataPoint in data:
-    names.append(dataPoint["user"])
+#names = []
+    # for dataPoint in data:
+    # names.append(dataPoint["user"])
 #cool, now let's be a bit more selective
-names.clear()
-for dataPoint in data:
-    likes= int(dataPoint["likes"])
-    if likes > 20:
-        names.append(dataPoint["user"])
-        #pprint(dataPoint) #I'm lazy
-#Ok, lets try figuring out what we'd want for a meme
-#Want to limit the number of sentences, or maybe the amount of text. For now we'll just limit ourselves to two sentences
-#also going to want an image somewhere in the picture, that's a bit tougher. Will start by looking for image extensions or tags from sites like imgur
-#Only looking for successful memes, so we're limiting ourselves to at least 20 retweets. Not sure exactly what's the sweet number here
-#but I'm going to prioritize propensity to share content over plain "likes".
-names.clear()
+# names.clear()
+# for dataPoint in data:
+#     likes= int(dataPoint["likes"])
+#     if likes > 20:
+#         names.append(dataPoint["user"])
+#         #pprint(dataPoint) #I'm lazy
+# #Ok, lets try figuring out what we'd want for a meme
+# #Want to limit the number of sentences, or maybe the amount of text. For now we'll just limit ourselves to two sentences
+# #also going to want an image somewhere in the picture, that's a bit tougher. Will start by looking for image extensions or tags from sites like imgur
+# #Only looking for successful memes, so we're limiting ourselves to at least 20 retweets. Not sure exactly what's the sweet number here
+# #but I'm going to prioritize propensity to share content over plain "likes".
+# names.clear()
 memeTweets = []
+pprint("start")
 for dataPoint in data:
     #Well for starters how do we count the number of sentences? ("..." is going to fuck up a split)
     #Start by just counting words, that probably matters more anyway. Might want to remove any non-consecutive periods anyway
     if int(dataPoint["retweets"])> 20:
         s = dataPoint["text"]
         memeWorthy = len(s.split())  # splitting on whitespace
-        if memeWorthy < 50: #does order impose performance cost?
+        if memeWorthy < 25: #does order impose performance cost?
             if any(substring in s for substring in substring_list): #https://stackoverflow.com/questions/8122079/python-how-to-check-a-string-for-substrings-from-a-list
                 memeTweets.append(dataPoint)
-                pprint(dataPoint)
+                #pprint(dataPoint)
     #This is ok but we're still going to end up with a ton of trash. Could bounce over to the URL and use twitter's API to parse the comments
     #for reactions like "hahaha", "meme", "laughing emoji" or "lmao dead" or something. Then we queue each tweet by the chance its something
     #we can make into a meme, and give that to the maker first
 
+pprint(len(memeTweets))
--- MemeMaker/MemeGenerator.py
+++ MemeMaker/MemeGenerator.py
@@ -3,10 +3,9 @@
 import requests
 from bs4 import BeautifulSoup
 import webbrowser
-import urllib
+import urllib.request
 
 #don't parse html with REGEX! https://stackoverflow.com/questions/1732348/regex-match-open-tags-except-xhtml-self-contained-tags
-i = 0
 
 #Going to need a method to follow a pic.twitter URL, find the actual image source, and return it to the user
 def picURLParser(URL):
@@ -15,25 +14,46 @@ def picURLParser(URL):
         print("requests are fucked")
     soup = BeautifulSoup(page.content, 'html.parser')
     # ho boy I sure do hope twitter's image embedding is standardized
-    picData = soup.find(id="permalink-overlay")
+    pageData = soup.find(id="permalink-overlay")
+    picData = pageData.find(class_="AdaptiveMediaOuterContainer")
     link = picData.find('img')
     URL = link.attrs['src'] #getting the wrong picture, gotta think about this some more
     return URL
 
 #Going to need a method that takes in a top string, bottom string, and image source, and passes them off to the meme maker. Might save
 #this image programatically, no clue how to do that
+
 def memeMaker(topString,bottomString,URL):
-    global i
-    # going to start by opening firefox, but eventually I want all this automated
-    memeURL = "https://memegen.link/custom/" +topString + "/" + bottomString + ".jpg?alt" "=" + URL #working but I've hardcoded the .jpg?alt part, no clue kinds of stuff I'll run into with this
-    webbrowser.open_new(memeURL) #If your OS loads Edge as the default browser you need to take a break and rethink your life
-    urllib.urlretrieve("http://www.digimouth.com/news/media/2011/09/google-logo.jpg", "TestImage_" + i + ".jpg")
-    i = i + 1 #python I love you but this makes me cry
-    #ideally
+    #try:
+        # going to start by opening firefox, but eventually I want all this automated
+        memeURL = "https://memegen.link/custom/" +topString + "/" + bottomString + ".jpg?alt" "=" + URL #working but I've hardcoded the .jpg?alt part, no clue kinds of stuff I'll run into with this
+        webbrowser.open_new(memeURL) #If your OS loads Edge as the default browser you need to take a break and rethink your life
+        filename = memeURL.split('/')[-1]
+        req = urllib.request.Request(memeURL, headers={'User-Agent': "Magic Browser"})
+        img = urllib.request.urlopen(req)
+        localFile = open(filename,'wb')
+        localFile.write(img.read())
+        localFile.close()
+
+
+    #catch
 #Method to split text into top and bottom meme? I don't know if its even worth the effort, maybe just for fun
-
-
+def textSplitter(text):
+    stringList = text.split()
+    length = len(stringList)
+    #I'm sure there's a better way to do this but this'll work. This way we know we're always getting whole words
+    firstString = ""
+    secondString = ""
+    #round up or down?
+    for x in range(0, int(length/2)):
+        firstString += stringList[x] + " "
+    for x in range(int(length/2), length):
+        secondString += stringList[x] + " "
+    splitString = [firstString,secondString]
+    return splitString
 
 #Main test stuff, this should be on top come on python why you gotta enforce ordering
+text = "i like really good memes"
+splitText = textSplitter(text)
 picURL = picURLParser("https://twitter.com/RT_com/status/942156129961877504/photo/1")
-memeMaker("i like", "really good memes", picURL)
+memeMaker(splitText[0], splitText[1], picURL)

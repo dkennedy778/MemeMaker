4cc8d87ea8038927eb01be9b7a0d8ace425a9fd9
dkennedy778 <derek.kennedy.13@cnu.edu>
2017-12-23 20:15:26 -0500

    Update MemeCompiler.py
    Improved the failure rate on tweet to meme conversion dramatically, hashtag characters were causing meme generation to fail

--- MemeMaker/MemeCompiler.py
+++ MemeMaker/MemeCompiler.py
@@ -1,5 +1,3 @@
-#All the things related to meme generation, might have to split this up
-#If I'm going to be networking I need a logger service too
 import requests
 from bs4 import BeautifulSoup
 import webbrowser
@@ -12,11 +10,11 @@ from PIL import ImageDraw
 #don't parse html with REGEX! https://stackoverflow.com/questions/1732348/regex-match-open-tags-except-xhtml-self-contained-tags
 
 
-#Going to need a method to follow a pic.twitter URL, find the actual image source, and return it to the user
+#Opens the tweet containing the picture and parses its URL out using BS4
 def picURLParser(URL):
     page = requests.get("http://" +URL)
     if(page.status_code != 200):
-        print("requests are fucked")
+        print("requests failed")
     soup = BeautifulSoup(page.content, 'html.parser')
     # ho boy I sure do hope twitter's image embedding is standardized
     pageData = soup.find(id="permalink-overlay")
@@ -25,16 +23,18 @@ def picURLParser(URL):
     URL = link.attrs['src'] 
     return URL
 
-#Going to need a method that takes in a top string, bottom string, and image source, and passes them off to the meme maker. Might save
-#this image programatically, no clue how to do that
-
+#Going to need a method that takes in a top string, bottom string, and image source, and passes them off to the meme maker.
 def memeMaker(topString,bottomString,URL):
     try:
         # going to start by opening firefox, but eventually I want all this automated
         parseMeBB = URL[0] + URL[1] + URL[2]
+        #Removing tweets which are just images, likely superflorus after fixes to JSONParser, test this
+        if (topString == "" or bottomString == ""):
+            return
         if(parseMeBB == "pic"):
             nonTwitterURL = picURLParser(URL)
-        memeURL = "https://memegen.link/custom/" +topString + "/" + bottomString + ".jpg?alt" "=" + nonTwitterURL #working but I've hardcoded the .jpg?alt part, no clue kinds of stuff I'll run into with this
+        memeURL = "https://memegen.link/custom/" +topString + "/" + bottomString + ".jpg?alt" "=" + nonTwitterURL #working, formatting issues abound
+        memeURL = memeURL.replace('#',"") # Hashtags will cause link to fail, may be able to handle this in format text. Arguable that tweets containing hashtags aren't suitable for memes anyway
         webbrowser.open_new(memeURL) #If your OS loads Edge as the default browser you need to take a break and rethink your life
         filename = memeURL.split('/')[-1]
         req = urllib.request.Request(memeURL, headers={'User-Agent': "Magic Browser"})
@@ -44,15 +44,16 @@ def memeMaker(topString,bottomString,URL):
         localFile.close()
         #webbrowser.close()
     except:
-        print("Unknown exception hit, make a logger service")
+        print("s")
 
-#Method to split text into top and bottom meme? I don't know if its even worth the effort, maybe just for fun
+#Method to split text into top and bottom to fit into the meme
 def textSplitter(text):
     stringList = text.split()
     length = len(stringList)
     #I'm sure there's a better way to do this but this'll work. This way we know we're always getting whole words
     firstString = ""
     secondString = ""
+    #round up or down?
     for x in range(0, int(length/2)):
         firstString += stringList[x] + " "
     for x in range(int(length/2), length):
@@ -65,14 +66,14 @@ def formatText(text):
     lazyBoi = re.sub(r"pic.twitter.com","",returnText)
     #re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', text)
     return lazyBoi
-
+#Main method
 def makeMemes(tweets, PicURLs):
     i = 0
     for tweet in tweets:
         text = tweet["text"]
-        rawText = formatText(text)
-        splitRawText = textSplitter(rawText)
+        formattedText = formatText(text)
+        splitFormattedText = textSplitter(formattedText)
         picURL = PicURLs[i]
-        memeMaker(splitRawText[0], splitRawText[1], PicURLs[i])
+        memeMaker(splitFormattedText[0], splitFormattedText[1], PicURLs[i])
         i = i +1 #python why
 

722c11ea1416494617ea95f7c81442a17654dac1
dkennedy778 <derek.kennedy.13@cnu.edu>
2018-01-20 14:08:51 -0500

    No longer including tweets with emojis

--- MemeMaker/JSONParser.py
+++ MemeMaker/JSONParser.py
@@ -1,11 +1,14 @@
 #sample of json parsing methods to act as a foundation for future twitterscraping projects. Assuming data is formatted
 #to twitterscraper's format
 
+
+import emoji #why does this exist
 import json
 from langdetect import detect
 from urlextract import URLExtract
 from pprint import pprint
 import logging
+import re
 
 JSONParser_logger = logging.getLogger('memeMaker')
 namesList = []
@@ -18,7 +21,7 @@ def parseMemes(filename):
     memeTweets = []
     pprint("start")
     extractor = URLExtract()
-    #Evaluating tweets on the following criteria 
+    #Evaluating tweets on the following criteria
     #1. The tweet has more than 20 retweets
     #2. The tweet has more than 4 words and less than 25
     #3. The tweet has some kind of image URL
@@ -27,24 +30,25 @@ def parseMemes(filename):
         for dataPoint in data:
             if int(dataPoint["retweets"])> 20:
                 s = str(dataPoint["text"])
-                if(detect(s) == 'en'):
-                    #if not re.search('[?-@-#]',s):
-                    memeWorthy = len(s.split())  # splitting on whitespace
-                    if memeWorthy < 25 and memeWorthy > 4: #does order impose performance cost?
-                        if any(substring in s for substring in substring_list): #https://stackoverflow.com/questions/8122079/python-how-to-check-a-string-for-substrings-from-a-list
-                            #loops through all URLs in the tweet until it finds the image one
-                            urls = extractor.find_urls(s)
-                            #HACK SOLUTION, need to alter a datastructure to allow for tweets that have 2+ picture URLs. This just includes the first
-                            if urls: #poorly formatted tweets
-                                for url in urls:
-                                    if any(substring in url for substring in substring_list):
-                                        #splitting the string to get ride of cases where the URL is more than just the pic.twitter link, probably should be doing this with regular expressions but whatever
-                                        memeURLList = str(url).split("pic")
-                                        picURL = "pic" + memeURLList[1]
-                                        picURLs.append(picURL)
-                                        memeTweets.append(dataPoint)
-                                        break
-                    #pprint(dataPoint)
+                if(text_has_no_emojis(s)):
+                    if(detect(s) == 'en'):
+                        memeWorthy = len(s.split())  # splitting on whitespace
+                        if memeWorthy < 25 and memeWorthy > 4: #does order impose performance cost?
+                            #if not re.search('[?-@-#]',s):
+                            if any(substring in s for substring in substring_list): #https://stackoverflow.com/questions/8122079/python-how-to-check-a-string-for-substrings-from-a-list
+                                #loops through all URLs in the tweet until it finds the image one
+                                urls = extractor.find_urls(s)
+                                #HACK SOLUTION, need to alter a datastructure to allow for tweets that have 2+ picture URLs. This just includes the first
+                                if urls: #poorly formatted tweets
+                                    for url in urls:
+                                        if any(substring in url for substring in substring_list):
+                                            #splitting the string to get ride of cases where the URL is more than just the pic.twitter link, probably should be doing this with regular expressions but whatever
+                                            memeURLList = str(url).split("pic")
+                                            picURL = "pic" + memeURLList[1]
+                                            picURLs.append(picURL)
+                                            memeTweets.append(dataPoint)
+                                            break
+                                            #pprint(dataPoint)
     except Exception as e:
         JSONParser_logger.exception("exception hit")
     return (memeTweets, picURLs)
@@ -53,17 +57,17 @@ def parseMemes(filename):
     #we can make into a meme, and give that to the maker first
 
 
-#Other sample parsing methods
+# -*- encoding: utf-8 -*-
+# pip install emoji
 
-#first lets get the names of all users
-#names = []
-    # for dataPoint in data:
-    # names.append(dataPoint["user"])
-#cool, now let's be a bit more selective
-# names.clear()
-# for dataPoint in data:
-#     likes= int(dataPoint["likes"])
-#     if likes > 20:
-#         names.append(dataPoint["user"])
-#         #pprint(dataPoint) #I'm lazy
+
+#This method sourced from https://gist.github.com/jezdez/0185e35704dbdf3c880f
+def char_is_emoji(character):
+    return character in emoji.UNICODE_EMOJI
+
+def text_has_no_emojis(text):
+    for character in text:
+        if character in emoji.UNICODE_EMOJI:
+            return False
+    return True
 

053906594c20c1b13ed754e88c2f6d7b5e1a67dd
dkennedy778 <derek.kennedy.13@cnu.edu>
2018-01-22 10:28:27 -0500

    Cleaned up comments

--- MemeMaker/JSONParser.py
+++ MemeMaker/JSONParser.py
@@ -33,35 +33,25 @@ def parseMemes(filename):
                 if(text_has_no_emojis(s)):
                     if(detect(s) == 'en'):
                         memeWorthy = len(s.split())  # splitting on whitespace
-                        if memeWorthy < 25 and memeWorthy > 4: #does order impose performance cost?
-                            #if not re.search('[?-@-#]',s):
+                        if memeWorthy < 25 and memeWorthy > 4: 
                             if any(substring in s for substring in substring_list): #https://stackoverflow.com/questions/8122079/python-how-to-check-a-string-for-substrings-from-a-list
                                 #loops through all URLs in the tweet until it finds the image one
                                 urls = extractor.find_urls(s)
-                                #HACK SOLUTION, need to alter a datastructure to allow for tweets that have 2+ picture URLs. This just includes the first
-                                if urls: #poorly formatted tweets
+                                if urls: 
                                     for url in urls:
                                         if any(substring in url for substring in substring_list):
-                                            #splitting the string to get ride of cases where the URL is more than just the pic.twitter link, probably should be doing this with regular expressions but whatever
+                                            #splitting the string to get ride of cases where the URL is more than just the pic.twitter link
                                             memeURLList = str(url).split("pic")
                                             picURL = "pic" + memeURLList[1]
                                             picURLs.append(picURL)
                                             memeTweets.append(dataPoint)
                                             break
-                                            #pprint(dataPoint)
     except Exception as e:
         JSONParser_logger.exception("exception hit")
     return (memeTweets, picURLs)
-    #This is ok but we're still going to end up with a ton of trash. Could bounce over to the URL and use twitter's API to parse the comments
-    #for reactions like "hahaha", "meme", "laughing emoji" or "lmao dead" or something. Then we queue each tweet by the chance its something
-    #we can make into a meme, and give that to the maker first
-
-
-# -*- encoding: utf-8 -*-
-# pip install emoji
-
 
 #This method sourced from https://gist.github.com/jezdez/0185e35704dbdf3c880f
+#Checks to see if text has emojis, if it does the tweet is excluded 
 def char_is_emoji(character):
     return character in emoji.UNICODE_EMOJI
 
